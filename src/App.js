48757b6743b257a46b41c2739cefbcdbeba446c3
LONEWOLF <131397613+LonewolfGan@users.noreply.github.com>
2025-12-22 14:06:02 +0100

    refresh solved

--- src/App.js
+++ src/App.js
@@ -5,21 +5,9 @@ import { CallToAction } from "./components/CallToAction";
 import { useScrollToTop } from "./hooks/useScrollToTop";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 export const App = () => {
   useScrollToTop();
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const redirect = urlParams.get('redirect');
-  if (redirect) {
-    navigate(redirect, { replace: true });
-  }
-}, [navigate]);
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 font-sans text-gray-900">
